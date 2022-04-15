e600988c76f80c23751693ddfb506043232d87e6
SergioVista <sergio.slvirtual@gmail.com>
2022-04-14 23:09:54 -0300

    Adicionando uma imagem
    Finalizei esta etapa, rever no próximo dia e seguir para próxima.

--- my-app/App.js
+++ my-app/App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Seguindo os passos da documentação!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo} />
+
+      <Text style={styles.instructions} >Seguindo os passos da documentação!</Text>
+
+      <Image source={{ uri: "https://i.imgur.com/yjXxFXQ.jpeg" }} style={styles.logo} />
+
+      <Text style={styles.instructions} >Seguindo os passos da documentação!</Text>
+
     </View>
   );
 }
@@ -18,4 +24,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+
 });
