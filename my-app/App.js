5774bf49ad9461509b6650892f52e5fffd57e112
SergioVista <sergio.slvirtual@gmail.com>
2022-04-14 22:12:59 -0300

    Documentação do EXPO
    Seguindo os passos da documentação expo.

--- my-app/App.js
+++ my-app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Calculo do Índice de Massa Muscular!!!</Text>
+      <Text>Seguindo os passos da documentação!</Text>
       <StatusBar style="auto" />
     </View>
   );
