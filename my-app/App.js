1fd92be5702d02d07910c8fe9f42a0bc609b7265
SergioVista <sergio.slvirtual@gmail.com>
2022-04-15 11:50:57 -0300

    atualizando EXPO SDK
    Erro ao tentar iniciar o projeto: Unable to find expo in this project - have you run yarn / npm install yet?
    Foi necessário atualizar o expo com o comando 'expo update' para funcionar.

--- my-app/App.js
+++ my-app/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import logo from './assets/logo.png'
+import { Image, StyleSheet, Text, TouchableOpacity,  View } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to acess camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectImage({localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
@@ -12,6 +43,10 @@ export default function App() {
       <Image source={{ uri: "https://i.imgur.com/yjXxFXQ.jpeg" }} style={styles.logo} />
 
       <Text style={styles.instructions} >Seguindo os passos da documentação!</Text>
+
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style = {styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
 
     </View>
   );
@@ -34,5 +69,18 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginHorizontal: 15,
   },
-
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+  },
 });
