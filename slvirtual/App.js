151b107eaa74e76f8a57d32076403b409b41020c
SergioVista <sergio.slvirtual@gmail.com>
2022-05-02 07:31:23 -0300

    update

--- slvirtual/App.js
+++ slvirtual/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import button from './components/Botao';
 
 export default function App() {
   return (
